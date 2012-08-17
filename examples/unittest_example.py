21b9c814213bf8de3547536b5bbf25a5ea65f17d
Ian O Connell <ian@knobout.com>
2012-08-17 10:07:13 -0700

    Fix example to use the right function

--- examples/unittest_example.py
+++ examples/unittest_example.py
@@ -23,19 +23,19 @@ def produce_not_match_output(tuple):
 
 class UnitTestExample(CascadingTestCase):
     def testWrapped(self):
-        results = self.get_outputs_for_inputs([33,'arg1 arg2 arg3'], wrapped_case)
+        results = self.run_udf([33,'arg1 arg2 arg3'], wrapped_case)
         self.assertEqual(3, len(results))
 
     def testUnWrapped(self):
-        results = self.get_outputs_for_inputs([33,'arg1 arg2 arg3'], unwrapped_case)
+        results = self.run_udf([33,'arg1 arg2 arg3'], unwrapped_case)
         self.assertEqual(3, len(results))
 
     def testProducesMatchesOutput(self):
-        results = self.get_outputs_for_inputs([33,'arg1 arg2 arg3'], wrapped_case, error_on_different_length_tuple = True)
+        results = self.run_udf([33,'arg1 arg2 arg3'], wrapped_case, error_on_different_length_tuple = True)
         self.assertEqual(3, len(results))
 
     def testProducesFailsToMatchOutput(self):
-        self.assertRaises(ProducesOutputMissMatchError, self.get_outputs_for_inputs, [33,'arg1 arg2 arg3'], produce_not_match_output, error_on_different_length_tuple = True)
+        self.assertRaises(ProducesOutputMissMatchError, self.run_udf, [33,'arg1 arg2 arg3'], produce_not_match_output, error_on_different_length_tuple = True)
 
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(UnitTestExample)
