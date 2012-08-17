876042db471d0184da5447aa73a66247b59ff9de
Ian O Connell <ian@knobout.com>
2012-08-17 13:47:30 -0700

    Handle deeper module paths, redirect stdout, stderr

--- python/pycascading/python_streaming_proxy.py
+++ python/pycascading/python_streaming_proxy.py
@@ -25,6 +25,12 @@ except ImportError, e:
     import argparse
     import simplejson as json
 
+stdin = sys.stdin
+stdout = sys.stdout
+sys.stdin = None
+sys.stdout = open('/tmp/stdout.txt', 'w')
+sys.stderr = open('/tmp/stderr.txt', 'w')
+
 
 parser = argparse.ArgumentParser(description='Handles dependency fetching and setting up of the python env, handling the streaming portion of the op')
 parser.add_argument('function', help='Function to call, must be in the PYTHONPATH')
@@ -41,15 +47,23 @@ for lib in args.userlibs:
 if len(needed_libs) > 0:
     install_with_easy_install(needed_libs)
 
-def full_path_to_function_module(function_module_path):
-    last_dot = string.rfind(function_module_path, ".")
-    module = function_module_path[0: last_dot]
-    function = function_module_path[last_dot +1:]
-    return (module, function)
+def get_user_function(function_path):
+    def full_path_to_function_module(function_module_path):
+        last_dot = string.rfind(function_module_path, ".")
+        module = function_module_path[0: last_dot]
+        first_dot = string.find(module, ".")
+        function_path = function_module_path[first_dot +1:]
+        return (module, function_path)
+    
+    module_str,function_path = full_path_to_function_module(function_path)
+    user_module = __import__(module_str)
+    current_location = user_module
+    for next_target in function_path.split("."):
+        current_location = getattr(current_location, next_target)
+    return current_location
 
-module_str,function_str = full_path_to_function_module(args.function)
-user_module = __import__(module_str)
-user_function = getattr(user_module, function_str)
+
+user_function = get_user_function(args.function)
 
 base_fields = None
 class Tuple(object):
@@ -64,14 +78,18 @@ class Tuple(object):
     def __iter__(self):
         return self.__args.__iter__()
 
+
+
 # input comes from STDIN (standard input)
-line = sys.stdin.readline()
+line = stdin.readline()
 while len(line) > 0:
     # remove leading and trailing whitespace
     line = line.strip()
     tuple = Tuple(json.loads(line))
     for result in user_function(tuple):
         res = json.dumps(result) 
-        print(res)
-    print '' # Seperator so parent knows we moved onto next tuple
-    line = sys.stdin.readline()
+        stdout.write(res)
+        stdout.write('\n')
+    stdout.write('\n') # Seperator so parent knows we moved onto next tuple
+    stdout.flush()
+    line = stdin.readline()
